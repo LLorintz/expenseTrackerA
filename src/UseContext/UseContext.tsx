db9a0c907f0a487efc17e1e228c862bd415ebf06
Lőrintz Levente <lorintz.levente@burg.lo.hu>
2024-11-27 08:56:32 +0100

    üzenet

--- src/UseContext/UseContext.tsx
+++ src/UseContext/UseContext.tsx
@@ -8,20 +8,18 @@ type transactioType={
 
 type GlobalState = {
     transactions:transactioType[];
-    deleteTransaction:(id:number)=>void
+    deleteTransaction:(id:number)=>void;
+    addTransaction:(t:transactioType)=>void;
 }
 
 const initialState = {
-    transactions:[
-        {id:1, text:'Flowers',amount:-30},
-        {id:2, text:'Salary',amount:300},
-        {id:3, text:'Books',amount:-10},
-    ]
+    transactions:[   ]
 }
 
 export const GlobalContext = createContext<GlobalState>({
     transactions:initialState.transactions,
-    deleteTransaction:()=>{}
+    deleteTransaction:()=>{},
+    addTransaction:()=>{},
 })
 
 type childreProps={
@@ -36,8 +34,12 @@ const GlobalProvider = ({children}:childreProps) => {
         setTransactions(transactions.filter(tranaction=>tranaction.id!==id))
     }
 
+    const addTransaction = (t:transactioType)=>{
+        setTransactions(prev=>[...prev,t])
+    }
+
   return (
-    <GlobalContext.Provider value={{transactions,deleteTransaction}}>
+    <GlobalContext.Provider value={{transactions,deleteTransaction,addTransaction}}>
         {children}
     </GlobalContext.Provider>
   )
--- src/components/AddTransaction.tsx
+++ src/components/AddTransaction.tsx
@@ -1,15 +1,43 @@
+import { FormEvent, useContext, useState } from "react"
+import { GlobalContext } from "../UseContext/UseContext"
 
 
 const AddTransaction = () => {
+
+  const {addTransaction} = useContext(GlobalContext)
+
+  const [transaction,setTransaction]=useState('')
+  const [transactionValue, setValue] = useState(0)
+
+  const handleTransactionChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    setTransaction(e.target.value)
+  }
+
+  const handlevalueChange =(e:React.ChangeEvent<HTMLInputElement>)=>{
+    setValue(parseInt(e.target.value))
+  }
+
+  const handleSubmit = (e:FormEvent)=>{
+    e.preventDefault();
+    const newTransaction = {
+      id:Math.floor(Math.random()*1000000),
+      text:transaction,
+      amount:transactionValue,
+    } 
+    addTransaction(newTransaction);
+    setTransaction('');
+    setValue(0);
+  }
+
   return (
-    <div className="addTr">
+    <form onSubmit={handleSubmit} className="addTr">
         <h2>Add Transaction</h2>
         <label htmlFor="text">Text</label>
-        <input type="text" placeholder="Enter text"/>
+        <input type="text" placeholder="Enter text" value={transaction} onChange={handleTransactionChange}/>
         <label htmlFor="amount">Amount (negative-expense, positive-income)</label>
-        <input type="number"/>
+        <input type="number" value={transactionValue} onChange={handlevalueChange}/>
         <button className="addBtn">Add Transaction</button>
-    </div>
+    </form>
   )
 }
 
